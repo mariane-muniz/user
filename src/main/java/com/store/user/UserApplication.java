7406fd08a5d608f025a7ba2866372f8fee599ebf
Mariane Muniz <mariane.muniz@star-it.com.br>
2019-05-01 11:51:25 -0300

    first commit

--- src/main/java/com/store/user/UserApplication.java
+++ src/main/java/com/store/user/UserApplication.java
@@ -2,14 +2,13 @@ package com.store.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
-public class DemoApplication {
+public class UserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		
+		SpringApplication.run(UserApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/store/user/controllers/UserController.java
@@ -0,0 +1,17 @@
+package com.store.user.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @ResponseBody
+    @RequestMapping
+    public String getUser() {
+
+        return "its works!";
+    }
+}
